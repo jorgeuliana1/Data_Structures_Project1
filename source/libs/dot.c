898ef623176589ee016ffe87fc3e7dbf6c72ed7b
jorgeuliana1 <ulianamjjorge@gmail.com>
2018-10-02 19:11:43 -0300

    Added commentaries.

--- source/libs/dot.c
+++ source/libs/dot.c
@@ -11,13 +11,31 @@ FILE * openGVFile() {
 }
 
 void createDot(Terminal * t, Router * r, FILE * f) {
+    /*
+    --VARIABLES--
+    t: Terminals list.
+    r: Routers list.
+    f: File where the graph will be printed at.
+    */
+
+    /*
+    If it is not the first time it calls the funtion "//intermediario" will
+    be printed.
+    */
     static int counter = 0;
     counter++;
     if(counter > 1) fprintf(f, "//intermediario\n\n");
+    /*
+    PRINTING STRICT GRAPH { AND INICIALIZING VARIABLES.
+    */
     fprintf(f, "strict graph {\n");
     Terminal * auxt = t;
     Router * auxr = r;
     Connect * auxc;
+    /*
+    While there is any connection it is going to print.
+    */
+    //First it prints the terminal-router connections.
     while(auxt != NULL) {
         if(thereIsTRConnection(auxt))
             fprintf(f, "\t%s -- %s;\n", terminalName(auxt), connectedRouterName(auxt));
@@ -25,6 +43,7 @@ void createDot(Terminal * t, Router * r, FILE * f) {
             fprintf(f,"\t%s;\n", terminalName(auxt));
         auxt = nextTerminal(auxt);
     }
+    //Then it prints the router-router connections.
     while(auxr != NULL) {
         if(thereIsRRConnection(auxr)) {
             auxc = getCNT(auxr);
@@ -33,9 +52,11 @@ void createDot(Terminal * t, Router * r, FILE * f) {
                 auxc = nextCNT(auxc);
             }
         }
+        //If the router hasn't any connection it will be printed alone.
         else
             fprintf(f, "\t%s;\n", routerName(auxr));
         auxr = nextRouter(auxr);
     }
+    //Printing the "}"
     fprintf(f, "}\n");
 }
