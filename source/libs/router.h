dedd4f23c1b5caff20d6b852d065fb5caba733fa
jorgeuliana1 <ulianamjjorge@gmail.com>
2018-10-02 21:12:39 -0300

    Added documentation.

--- source/libs/router.h
+++ source/libs/router.h
@@ -1,74 +1,71 @@
 #ifndef ROUTER_H
 #define ROUTER_H
 #include "connection.h"
+/*
+--ABOUT THIS LIBRARY--
+It is responsible for the Router type functions.
+*/
 
 typedef struct router Router;
 
 /*
---inicializeRouter--
-Precondition: Router is not inicilized.
-Input: Not inicialized router.
-Output: Inicialized router.
-Postcondition: Router inicilized.
+--Inicialize router--
+Precondition: Router list is not inicilized.
+Input: Not inicialized router list.
+Output: Inicialized router list.
+Postcondition: Router list is inicilized.
 */
 Router * inicializeRouters();
 
 /*
---findRouter--
-Precondition: Router list is inicialized and with at least a element inside.
-Input: Router list and the searched Router's name.
-Output: Router with said name.
+--Find router by name--
+Precondition: Router list is inicialized and with at least one element
+inside it.
+Input: Router list and the target router name.
+Output: Target router.
 Postcondition: None.
 */
 Router * findRouter(Router * rlist, char * name);
 
 /*
---registerRouter--
-Precondition: Router * r is not inicialized.
-Input: Not inicialized router, router name and router carrier name.
+--Register router--
+Precondition: Router is inicialized.
+Input: Inicialized router list, router name, carrier name and log file.
 Output: Updated router list.
-Postcondition: Router is inicialized and on the list.
+Postcondition: Router is inicialized and included at the list.
 */
 Router * registerRouter(Router * r, char * n, char * o, FILE * l);
 
 /*
---removeRouter--
-Precondition: Router and terminal are inicilized.
-Input: Router and terminal lists and router name.
-Output: List with removed router.
+--Remove router--
+Precondition: Router exists.
+Input: Router list and router name, log file and confirmation if the user
+wants to print the messages on the file.
+Output: Updated router list.
 Postcondition: Router is removed from the network.
 */
 Router * removeRouter(Router * rlist, char * rn, FILE * l, int veriFile);
 
 /*
---carrierFrequency--
+--Carrier frequency--
 Precondition: Router list is inicialized.
-Input: Router list and name of the carrier.
+Input: Router list, name of the carrier and output file.
 Output: None.
-Postcondition: The number of routers from said carrier.
+Postcondition: The number of routers with said carrier printed.
 */
 void carrierFrequency(Router * rlist, char * carrier, FILE * o);
 
 /*
---routerName--
-Precondition: 'r' is an inicialized router.
-Input: The router with the requested name.
-Output: The router's name.
+--Router name--
+Precondition: Router exists.
+Input: Router.
+Output: String with the router name.
 Postcondition: None.
 */
 char * routerName(Router * r);
 
 /*
---printRouters--
-Precondition: Router list is inicialized.
-Input: Inicialized Router.
-Output: None.
-Postcondition: Routers info is printed.
-*/
-void printRouters(Router * r);
-
-/*
---decimateRouters--
+--Decimate routers--
 Precondition: Router list is inicialized.
 Input: Inicialized router list.
 Output: Empty router list.
@@ -76,26 +73,95 @@ Postcondition: All the routers are destroyed.
 */
 Router * decimateRouters(Router * r);
 
+/*
+--Web-connect routers--
+Precondition: Router list is inicialized.
+Input: Router list, name of the routers, log file and log file verification.
+Output: Updated router list.
+Postcondition: Routers are connected.
+*/
 Router * webConnectRouters(Router * rlist, char * rn1, char * rn2, FILE * l, int veriFile);
 
+/*
+--Web-disconnect routers--
+Precondition: Router list is inicialized.
+Input: Router list, name of the routers, log file and log file verification.
+Output: Updated router list.
+Postcondition: Routers are disconnected.
+*/
 Router * webDisconnectRouters(Router * rlist, char * rn1, char * rn2, FILE * l, int veriFile);
 
+/*
+--There is router-router connection--
+Precondition: Router exists.
+Input: Router.
+Output: 1 if the router is connected to another router and 0 if it is not.
+Postcondition: None.
+*/
 int thereIsRRConnection(Router * r);
 
+/*
+--Get connection--
+Precondition: Router list is inicialized.
+Input: Router.
+Output: Router connection.
+Postcondition: None.
+*/
 Connect * getCNT(Router * r);
 
+/*
+--Next router--
+Precondition: Router exists.
+Input: Router.
+Output: Next router of the list.
+Postcondition: None.
+*/
 Router * nextRouter(Router * r);
 
+/*
+--Routers graph search--
+Precondition: Router list is inicialized.
+Input: Inicialized router list, origin router, target router name.
+Output: 1 if it is possible to access the target router from the origin router
+and 0 if it is not possible to do it.
+Postcondition: None.
+*/
 int routersGraphSearch(Router * rlist, Router * ro, char * rname2);
 
+/*
+--Unflag router--
+Precondition: Router exists.
+Input: Inicialized router list and target router name.
+Output: None.
+Postcondition: Target router is unflagged.
+*/
 void unflagRouter(Router * r, char * rn);
 
+/*
+--Flag router--
+Precondition: Router exists.
+Input: Inicialized router list and target router name.
+Output: None.
+Postcondition: Target router is flagged.
+*/
 void flagRouter(Router * r, char * rn);
 
+/*
+--Unflag all routers--
+Precondition: Router list is inicilized.
+Input: Inicialized router list.
+Output: None.
+Postcondition: All the routers of the list are unflagged.
+*/
 void unflagAllRouters(Router * r);
 
+/*
+--Is flagged--
+Precondition: Router exists.
+Input: Inicialized router list and target router name.
+Output: 1 if the target router is flagged and 0 if it isn't flagged.
+Postcondition: None.
+*/
 int isFlagged(Router * r, char * rn);
-
-//void printRouterAndConnections(Router * r);
 
 #endif
