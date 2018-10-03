8335477635df962438c70b180c2463aefe225871
jorgeuliana1 <ulianamjjorge@gmail.com>
2018-10-02 21:23:20 -0300

    Added documentation.

--- source/libs/output.h
+++ source/libs/output.h
@@ -1,11 +1,43 @@
 #ifndef OUTPUT_H
 #define OUTPUT_H
+/*
+--ABOUT THIS LIBRARY--
+It is responsible for the execution of the script contained in input.txt.
+*/
 
+/*
+--Start log file--
+Precondition: Log file is not inicilized.
+Input: None.
+Output: Inicialized log file.
+Postcondition: Log file is inicialized.
+*/
 FILE * startLogFile();
 
+/*
+--Start output file--
+Precondition: Output file is not inicilized.
+Input: None.
+Output: Inicialized output file.
+Postcondition: Output file is inicialized.
+*/
 FILE * startOutputFile();
 
+/*
+--Close file--
+Precondition: File is inicilized.
+Input: File to be closed.
+Output: Closed file.
+Postcondition: File is closed.
+*/
 FILE * closeFile(FILE * a);
 
+/*
+--Run script--
+Precondition: All the files are inicialized.
+Input: input.txt, log.txt, saida.txt.
+Output: Inicialized output file.
+Postcondition: Output file is inicialized.
+*/
 void runScript(FILE * f, FILE * logFile, FILE * output);
 #endif
